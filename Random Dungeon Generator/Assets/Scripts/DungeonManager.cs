70528f91df2f4dcd8ef95095612b9aaffa49b4d9
Kevin McNeil <dogwoodrawks@gmail.com>
2021-02-20 15:43:57 -0800

    not finished but close

--- Random Dungeon Generator/Assets/Scripts/DungeonManager.cs
+++ Random Dungeon Generator/Assets/Scripts/DungeonManager.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum DungeonType { Caverns, Rooms, Winding }
+
 public class DungeonManager : MonoBehaviour
 {
-    public GameObject[] randomItems;
+    public GameObject[] randomItems, randomEnemies;
     public GameObject floorPrefab, wallPrefab, tilePrefab, exitPrefab;
     public int totalFloorCount;
     [Range(0,100)] public int itemSpawnPercent;
+    [Range(0,100)] public int enemySpawnPercent;
+    //public bool useRoundedEdges;
+    public DungeonType dungeonType;
+    [Range(0,100)] public int windingHallPercent;
+    
 
    [HideInInspector] public float minX, maxX, minY, maxY;
 
@@ -19,6 +26,13 @@ public class DungeonManager : MonoBehaviour
    {
        floorMask = LayerMask.GetMask("Floor");
        wallMask = LayerMask.GetMask("Wall");
+       switch(dungeonType)
+       {
+           case DungeonType.Caverns: RandomWalker(); break;
+           case DungeonType.Rooms: RoomWalker(); break;
+           case DungeonType.Winding: WindingWalker(); break;
+
+       }
        RandomWalker();
    }
    void Update()
@@ -35,51 +49,153 @@ public class DungeonManager : MonoBehaviour
        floorList.Add(curPos);
        while(floorList.Count < totalFloorCount)
        {
-            switch(Random.Range(1, 5))
-                {
-                    case 1:
-                        curPos += Vector3.up;
-                        break;
-                    case 2:
-                        curPos += Vector3.right;
-                        break;
-                       case 3:
-                        curPos += Vector3.down;
-                        break;
-                    case 4:
-                        curPos += Vector3.left;
-                        break;
-                }
-            bool inFloorList = false;
-            for(int i = 0; i<floorList.Count; i++)
-                {
-                    if(Vector3.Equals(curPos, floorList[i]))
-                    {
-                        inFloorList = true;
-                        break;
-                    }
-                }   
-            if(!inFloorList)
+            curPos += RandomDirection();
+ 
+            if(!InFloorList(curPos))
                 {
                     floorList.Add(curPos);
                 }
        }
 
-       for(int i = 0; i<floorList.Count; i++)
+
+        StartCoroutine(DelayProgress());
+   }
+
+    void RoomWalker()
+    {
+        Vector3 curPos = Vector3.zero;
+       floorList.Add(curPos);
+       while(floorList.Count < totalFloorCount)
+       {
+           Vector3 walkDir = RandomDirection();
+           int walkLength = Random.Range(9, 18);
+
+            for(int i=0; i< walkLength; i++)
+            {
+                if(!InFloorList(curPos + walkDir))
+                    {
+                        floorList.Add(curPos + walkDir);
+                    }
+                curPos += walkDir;
+            }
+            RandomRoom(curPos);
+       }
+
+        StartCoroutine(DelayProgress());
+    }
+
+
+//    void RandomWalker()
+//    {
+//        Vector3 curPos = Vector3.zero;
+//        floorList.Add(curPos);
+//        while(floorList.Count < totalFloorCount)
+//        {
+//             curPos += RandomDirection();
+ 
+//             if(!InFloorList(curPos))
+//                 {
+//                     floorList.Add(curPos);
+//                 }
+//        }
+
+
+//         StartCoroutine(DelayProgress());
+//    }
+
+    void WindingWalker()
+    {
+        Vector3 curPos = Vector3.zero;
+       floorList.Add(curPos);
+       while(floorList.Count < totalFloorCount)
+       {
+            curPos = takeAHike(curPos);
+            int roll = Random.Range(0, 101);
+            if(roll > windingHallPercent){
+                RandomRoom(curPos);
+            }
+            
+       }
+
+        StartCoroutine(DelayProgress());
+    }
+
+Vector3 takeAHike(Vector3 myPos )
+{
+               Vector3 walkDir = RandomDirection();
+           int walkLength = Random.Range(9, 18);
+
+            for(int i=0; i< walkLength; i++)
+            {
+                if(!InFloorList(myPos + walkDir))
+                    {
+                        floorList.Add(myPos+ walkDir);
+                    }
+                myPos += walkDir;
+            }
+    return myPos;
+}
+ void RandomRoom(Vector3 myPos)
+ {
+                 //random room at end of long wlak
+            int width = Random.Range(1,5);
+            int height = Random.Range(1, 5);
+            for(int w = -width; w<= width; w++)
+            {
+                for(int h = -height; h <= height; h++)
+                {
+                 Vector3 offSet = new Vector3(w, h, 0);
+                 if(!InFloorList(myPos + offSet))
+                    {
+                        floorList.Add(myPos + offSet);
+                    }   
+                }
+            }
+ }
+
+
+    bool InFloorList(Vector3 myPos)
+    {
+     for(int i = 0; i<floorList.Count; i++)
+        {
+            if(Vector3.Equals(myPos, floorList[i]))
+            {
+                return true;
+            }
+        }  
+        return false;
+    }
+
+    Vector3 RandomDirection()
+    {
+         switch(Random.Range(1, 5))
+                {
+                    case 1:
+                        return Vector3.up;
+                    case 2:
+                        return Vector3.right;
+                       case 3:
+                        return Vector3.down;
+                    case 4:
+                        return Vector3.left;
+                }
+                return Vector3.zero;
+    }
+
+   IEnumerator DelayProgress()
+   {
+        for(int i = 0; i<floorList.Count; i++)
        {
            GameObject goTile = Instantiate(tilePrefab, floorList[i], Quaternion.identity) as GameObject;
            goTile.name = tilePrefab.name;
            goTile.transform.SetParent(transform);
        }
-        StartCoroutine(delayProgress());
-   }
-   IEnumerator delayProgress()
-   {
+
        while(FindObjectsOfType<TileSpawner>().Length >0)
        {
            yield return null;
        }
-       ExiltDoorway();
+       ExiltDoorway();  
        Vector2 hitSize = Vector2.one * 0.8f;
        for(int x = (int)minX - 2; x  <= (int)maxX + 2; x++)
        {
@@ -94,25 +210,48 @@ public class DungeonManager : MonoBehaviour
                        Collider2D hitRight = Physics2D.OverlapBox(new Vector2(x + 1, y), hitSize, 0, wallMask);
                        Collider2D hitBottom = Physics2D.OverlapBox(new Vector2(x, y - 1), hitSize, 0, wallMask);
                        Collider2D hitLeft = Physics2D.OverlapBox(new Vector2(x - 1, y), hitSize, 0, wallMask);
-                       if((hitTop || hitRight || hitBottom || hitLeft) && !(hitTop && hitBottom) && !(hitLeft && hitRight))
-                       {
-                           int roll = Random.Range(0,101);
-                           if(roll <= itemSpawnPercent)
-                           {
-                                int itemIndex = Random.Range(0, randomItems.Length);
-                                GameObject goItem = Instantiate(randomItems[itemIndex], hitFloor.transform.position, Quaternion.identity) as GameObject;
-                                goItem.name = randomItems[itemIndex].name;
-                                goItem.transform.SetParent(hitFloor.transform);
-                           }
-                       }
+                       RandomItems(hitFloor, hitTop, hitRight, hitBottom, hitLeft);
+                       RandomEnemies(hitFloor, hitTop, hitRight, hitBottom, hitLeft);
                    }
                }
            }
        }
    }
+
+    void RandomEnemies(Collider2D hitFloor, Collider2D hitTop, Collider2D hitRight, Collider2D hitBottom, Collider2D hitLeft)
+    {
+        if (hitFloor.transform.position == Vector3.zero) { return; };
+        if(!hitTop && !hitRight && !hitBottom && !hitLeft)
+        {
+            int roll = Random.Range(1,101);
+            if(roll <= enemySpawnPercent)
+            {
+                int enemyIndex = Random.Range(0, randomEnemies.Length);
+                GameObject goEnemy = Instantiate(randomEnemies[enemyIndex], hitFloor.transform.position, Quaternion.identity) as GameObject;
+                goEnemy.name = randomEnemies[enemyIndex].name;
+                goEnemy.transform.SetParent(hitFloor.transform);
+            }
+        }
+    }
+
+    void RandomItems(Collider2D hitFloor, Collider2D hitTop, Collider2D hitRight, Collider2D hitBottom, Collider2D hitLeft)
+    {
+        if((hitTop || hitRight || hitBottom || hitLeft) && !(hitTop && hitBottom) && !(hitLeft && hitRight))
+        {
+            int roll = Random.Range(1,101);
+            if(roll <= itemSpawnPercent)
+            {
+                int itemIndex = Random.Range(0, randomItems.Length);
+                 GameObject goItem = Instantiate(randomItems[itemIndex], hitFloor.transform.position, Quaternion.identity) as GameObject;
+                goItem.name = randomItems[itemIndex].name;
+                 goItem.transform.SetParent(hitFloor.transform);
+            }
+        }
+    }
+
    void ExiltDoorway()
    {
-       Vector3 doorPos = floorList[floorList.Count -1];
+        Vector3 doorPos = floorList[floorList.Count -1];
         GameObject goDoor = Instantiate(exitPrefab, doorPos, Quaternion.identity) as GameObject;
         goDoor.name = exitPrefab.name;
         goDoor.transform.SetParent(transform);
